e07ef1f1df6b55af91a3d96c2f5e514b34561444
Taylor Cummings <tdcummings5@gmail.com>
2022-12-04 14:12:19 -0600

    commit tree d311e25e769ab85de0604402db80301e395ba561

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    if (pickUpLocation < 42) {
+        
+        return 42-pickUpLocation
+    } else {
+      return  pickUpLocation-42
+    }
+    //returns the number of blocks given a value
+}
+
+function distanceFromHqInFeet(pickUpLocation) {
+    distanceFromHqInBlocks();
+    return distanceFromHqInFeet-pickUpLocation
+}
